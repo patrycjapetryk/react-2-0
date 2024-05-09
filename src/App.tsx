40165e57c6b1604746bee10ec89c7991666d5eff
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2024-05-09 16:56:50 +0200

    Remove React.Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { ComponentsGenerator } from "./components/ComponentsGenerator";
 
 function App() {
   return (
-    <>
-      <main className="flex min-h-screen w-full flex-col items-center gap-6 pt-20">
-        <ComponentsGenerator />
-      </main>
-    </>
+    <main className="flex min-h-screen w-full flex-col items-center gap-6 pt-20">
+      <ComponentsGenerator />
+    </main>
   );
 }
 
