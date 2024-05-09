bd4bb1be4741608fe139f83d0e79d26fbd50283e
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2024-05-09 11:31:15 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='flex justify-center items-center w-full min-h-screen'>
+        <p>Hello!!!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
